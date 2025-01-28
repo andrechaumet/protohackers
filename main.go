b8349d63dd5ed85f173a785c7309f15ca87a0ddb
andrechaumet <andrechaumetcutuli@gmail.com>
2025-01-27 23:46:56 -0300

    Added challenge no. 1 Prime Time

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	ln := setup(":8080")
+	defer ln.Close()
+	handleConns(ln)
+}
+
+func setup(address string) net.Listener {
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ln
+}
+
+func handleConns(ln net.Listener) {
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, err := readConn(conn, buf)
+		if err != nil {
+			log.Println("Error reading:", err)
+			return
+		}
+		if err := writeConn(conn, buf[:n]); err != nil {
+			log.Println("Error writing:", err)
+			return
+		}
+	}
+}
+
+func readConn(conn net.Conn, buf []byte) (int, error) {
+	n, err := conn.Read(buf)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return n, err
+	}
+	return n, nil
+}
+
+func writeConn(conn net.Conn, data []byte) error {
+	_, err := conn.Write(data)
+	return err
+}
